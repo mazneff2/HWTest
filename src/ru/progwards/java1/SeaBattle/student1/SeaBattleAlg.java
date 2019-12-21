39f27216e92d102448d622dc1aab34f2d95cb20f
Progwards <hwtest@progwards.ru>
2019-12-22 01:52:41 +0300

    SeaBattleAlg developed

--- src/ru/progwards/java1/SeaBattle/student1/SeaBattleAlg.java
+++ src/ru/progwards/java1/SeaBattle/student1/SeaBattleAlg.java
@@ -1,4 +1,7 @@
-package ru.progwards.java1.SeaBattle.student1;
+package ru.progwards.java1.SeaBattle.vpupkin;
+
+import java.util.Arrays;
+import java.util.Random;
 
 import ru.progwards.java1.SeaBattle.SeaBattle;
 import ru.progwards.java1.SeaBattle.SeaBattle.FireResult;
@@ -30,22 +33,195 @@ public class SeaBattleAlg {
     //         8|X|.|.|.|.|.|.|X|.|.|
     //         9|X|.|.|.|X|.|.|.|.|.|
 
-    public void battleAlgorithm(SeaBattle seaBattle) {
-        // пример алгоритма:
-        // стрельба по всем квадратам поля полным перебором
-        for (int y = 0; y < seaBattle.getSizeX(); y++) {
-        	for (int x = 0; x < seaBattle.getSizeY(); x++) {
-        		SeaBattle.FireResult fireResult = seaBattle.fire(x, y);
+	private static final int HORISONTAL = 0b01;
+	private static final int VERTIKAL = 0b10;
+	char[][] field;
+	SeaBattle seaBattle;
+	int hits;
+	int direction;
+	int fireDirection;
+	Random random = new Random();
+	public static boolean printField = true;
+	
+	void init(SeaBattle seaBattle) {
+    	this.seaBattle = seaBattle;
+    	field = new char[seaBattle.getSizeX()][];
+    	for (int x = 0; x < seaBattle.getSizeX(); x++) {
+    		field[x] = new char[seaBattle.getSizeY()];
+    		Arrays.fill(field[x], ' ');
+    	}
+	}
+	
+	void print() {
+		for (int y = 0; y < seaBattle.getSizeY(); y++) {
+			String str = "|";
+			for (int x = 0; x < seaBattle.getSizeX(); x++) {
+				str += field[x][y] + "|";
+			}
+			System.out.println(str);
+		}
+		System.out.println("----------------------");
+	}
+	
+	void checkShip(int x, int y) {
+		if ((direction&HORISONTAL) != 0) {
+			fireDirection = HORISONTAL;
+			checkDirection(fire(x-1, y));
+			if ((direction&HORISONTAL) != 0)
+				checkDirection(fire(x+1, y));
+		}
+		if ((direction&VERTIKAL) != 0) {
+			fireDirection = VERTIKAL;
+			if ((direction&VERTIKAL) != 0)
+				checkDirection(fire(x, y-1));
+			if ((direction&VERTIKAL) != 0)
+				checkDirection(fire(x, y+1));
+		}
+	}
+	
+	void checkDirection(SeaBattle.FireResult result) {
+		switch(result) {
+			case DESTROYED :
+				direction = 0;
+				break;
+			case HIT:
+				direction = fireDirection;
+				break;
+			case MISS:;
+		}
+	}
+	
+	void markField(int x, int y, SeaBattle.FireResult result) {
+		if (result != SeaBattle.FireResult.MISS)
+			field[x][y] = 'X';
+		else 
+			field[x][y] = '*';
+	}
+	
+	void checkHit(SeaBattle.FireResult result) {
+		if (printField)
+			print();
+		if (result != SeaBattle.FireResult.MISS)
+			hits++;
+	}
+	
+	void mark(int x, int y) {
+		if(x<0 || y<0 || x>=seaBattle.getSizeX() || y>=seaBattle.getSizeY())
+			return;
+		if (field[x][y] == ' ')
+			field[x][y] = '.';
+	}
+	
+	void markHit(int x, int y) {
+		mark(x-1, y-1);
+		mark(x-1, y);
+		mark(x+1, y+1);
+		mark(x, y+1);
+		mark(x+1, y-1);
+		mark(x+1, y);
+		mark(x-1, y+1);
+		mark(x, y+1);
+	}
+	
+	void markDestroyed() {
+    	for (int y = 0; y < seaBattle.getSizeY(); y++) {
+        	for (int x = 0; x < seaBattle.getSizeX(); x++) {
+        		if (field[x][y] == 'X')
+        			markHit(x, y);
+        	}
+    	}
+	}
+	
+	SeaBattle.FireResult fire(int x, int y) {
+		if(x<0 || y<0 || x>=seaBattle.getSizeX() || y>=seaBattle.getSizeY() || hits >= 20)
+			return SeaBattle.FireResult.MISS;
+		if (field[x][y] == ' ') {
+			SeaBattle.FireResult result = seaBattle.fire(x, y);
+			markField(x, y, result);
+			checkHit(result);
+			if (result == SeaBattle.FireResult.HIT) {
+				direction = fireDirection;
+				checkShip(x, y);
+			}
+			else
+				if (result == SeaBattle.FireResult.DESTROYED)
+					markDestroyed();
+			
+			return result;
+		}
+		return SeaBattle.FireResult.MISS;
+	}
+
+	int getRandom() {
+		double d = random.nextDouble();
+		return (int)Math.floor(d*10);
+	}
+	
+    // пример алгоритма:
+    // стрельба по всем квадратам поля полным перебором
+	void algorithm1() {
+    	for (int y = 0; y < seaBattle.getSizeY(); y++) {
+        	for (int x = 0; x < seaBattle.getSizeX(); x++) {
+        		fireDirection = VERTIKAL | HORISONTAL;
+        		fire(x, y);
             }
         }
+	}
+
+	void algorithm2() {
+		while(hits < 20) {
+    		fireDirection = VERTIKAL | HORISONTAL;
+			fire(getRandom(), getRandom());
+		}
+	}
+	
+	void stepFire(int offset) {
+    	for (int y = 0; y < seaBattle.getSizeY(); y++) {
+        	for (int x = y+offset; x < seaBattle.getSizeX(); x+=4) {
+        		fireDirection = VERTIKAL | HORISONTAL;
+        		fire(x, y);
+            }
+        	for (int x = y-offset; x >= 0; x-=4) {
+        		fireDirection = VERTIKAL | HORISONTAL;
+        		fire(x, y);
+            }
+    	}
+	}
+	
+	void algorithm3() {
+        	stepFire(0);
+        	stepFire(2);
+        	stepFire(1);
+        	stepFire(3);
+        }
+	
+    public void battleAlgorithm(SeaBattle seaBattle) {
+    	init(seaBattle);
+    	algorithm3();
     }
 
+    static void fullTest() {
+    	SeaBattleAlg.printField = false;
+    	double result = 0;
+    	for(int i=0; i<1000; i++) {
+    		SeaBattle seaBattle = new SeaBattle();
+    		new SeaBattleAlg().battleAlgorithm(seaBattle);
+    		result += seaBattle.getResult();
+    	}
+    	System.out.println(result/1000);
+    }
+    
+    static void oneTest() {
+    	SeaBattleAlg.printField = true;
+		SeaBattle seaBattle = new SeaBattle(true);
+		new SeaBattleAlg().battleAlgorithm(seaBattle);
+		System.out.println(seaBattle.getResult());
+	}
+    
     // функция для отладки
     public static void main(String[] args) {
     	System.out.println("Sea battle");
-    	SeaBattle seaBattle = new SeaBattle(true);
-    	new SeaBattleAlg().battleAlgorithm(seaBattle);
-    	System.out.println(seaBattle.getResult());
+    	fullTest();
     }
 }
 
