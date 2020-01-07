fd65f1010676bdc1c290deae27e30a75bc653ac4
Progwards <hwtest@progwards.ru>
2020-01-07 21:09:20 +0300

    Create Task2.java
    HW2

---
+++ src/ru/progwards/java1/lessons/helloworld/Task2.java
@@ -0,0 +1,19 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task2 {
+    public static void subtraction(int x, int y) {
+        System.out.print("Вызвана функция subtraction() с параметрами x = ");
+        System.out.print(x);
+        System.out.print(", y = ");
+        System.out.print(y);
+        System.out.println();
+
+        int z = x - y;
+        System.out.print("Результат z = ");
+        System.out.println(z);
+    }
+
+    public static void main(String[] args) {
+        subtraction(89, 55);
+    }
+}
