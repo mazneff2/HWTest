5be262f5ad513940191080e0ceec8b57651c3def
Progwards <hwtest@progwards.ru>
2020-01-07 20:54:13 +0300

    Create Task1.java
    HW

---
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -0,0 +1,20 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String a = "Хорошо идут дела";
+        String b = "Изучаю Java я!";
+        String с = " ";
+
+        System.out.println(a);
+        System.out.println(b);
+
+        //System.out.print(a);
+        //System.out.print(с);
+        //System.out.println(b);
+
+        System.out.print(b);
+        System.out.print(с);
+        System.out.println(a);
+    }
+}
