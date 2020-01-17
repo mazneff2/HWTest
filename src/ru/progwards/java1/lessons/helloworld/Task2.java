b945981bdefc54198cbc745370b9179c78db74a3
Progwards <hwtest@progwards.ru>
2020-01-17 08:55:16 +0300

    test
    test

--- src/ru/progwards/java1/lessons/helloworld/Task2.java
+++ src/ru/progwards/java1/lessons/helloworld/Task2.java
@@ -3,7 +3,7 @@ package ru.progwards.java1.lessons.helloworld;
 public class Task2 {
     public static void subtraction(int x, int y) {
         System.out.print("Вызвана функция subtraction() с параметрами x = ");
-        System.out.print(x);
+        System.out.print(x+x);
         System.out.print(", y = ");
         System.out.print(y);
         System.out.println();
