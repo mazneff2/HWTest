d6ae1a5a92eda31d239bf4e7e560a4120610b5d6
Progwards <hwtest@progwards.ru>
2020-01-17 09:01:26 +0300

    test

--- src/ru/progwards/java1/lessons/helloworld/Task1.java
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.helloworld;
 
 public class Task1 {
     public static void main(String[] args) {
-        String a = "Хорошо идут дела--";
+        String a = "Хорошо идут дела";
         String b = "Изучаю Java я!";
         String с = " ";
 
